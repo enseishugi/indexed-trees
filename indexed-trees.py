f1cb151c3abd05bb4207db50ebdc14a309a59c87
enseishugi <44185836+enseishugi@users.noreply.github.com>
2020-02-02 20:21:07 +0100

    Restructured the entire thing
    I almost entirely changed the script. Old functions are deleted, except for `prompt_command`, and I tried to give the script a more linear structure making it more explicit how the new functions interact.

--- indexed-trees.py
+++ indexed-trees.py
@@ -10,119 +10,197 @@ dall'utente.
 
 """
 
-def print_trees(tree_model, values):
+# ====================
+# Functons asking data
+# ====================
+    
+def ask_section():
+    """
+    """
+
+    print("Insert: section_type @ section_number @ section_name (separated by @)")
+    section = input("> ").split("@")
+
+    return section
+
+
+def ask_contents():
+    """
+    """
+
+    tree_contents = {}
+    
+    print("Insert: block name (if not, leave blank)")
+    tree_contents['block'] = input("> ")
+    
+    print("Insert: files path @ file extension (if not, leave blank)")
+    tree_contents['files'] = input("> ").split("@")
+
+    return tree_contents
+
+    
+def ask_printing_data():
+    """
+    """
+
+    print("Insert: number of iterations @ depth (separated by @)")
+    number_iterations, depth = input("> ").split("@")
+    dim = 1
+
+    return number_iterations, depth, dim
+
+
+# ==============================
+# Functions making string models
+# ==============================
+
+def make_heading(section):
+    """
+    """
+
+    heading = "%s %s - %s\n" % tuple(section)
+    return heading
+
+
+def make_content(content_type, content_data, section):
+    """
+    """
+
+    content = ''
+    dim = 0
+
+    if content_type == 'block':
+        content += '+begin_%i\n+end_%i\n' % (content_data, content_data)
+        dim += 1
+
+    if content_type == 'files':
+        path = content_data[0]
+        extension = content_data[1]
+        
+        # Makes section name lowercase, deletes punctuation, and replaces spaces by em dashes
+        section_type = section[0].lower()
+        section_number = section[1].lower()
+        section_name = section[2].translate({ord(c): None for c in ',;.:'}).lower().replace(' ', '-')
+        iterator = '%i'
+
+        exercise_filename = 'exercise-%s-%s.%s' % (section_number, iterator, extension)
+        solution_folder = '%s-%s-%s/' % (section_type, section_number, section_name)
+
+        exercise_path = 'Exercise: file:%s%s.%s\n' % (path, exercise_filename, extension)
+        solution_path = 'Solution: file:%s/\n' % solution_folder
+        
+        content += "%s%s" % (exercise_content, solution_content)
+        dim += 1
+
+    return content, dim
+        
+
+def make_tree_model(section, tree_contents, label):
+    """
+    """
+
+    iterator = "%i"
+    dim = 1
+    
+    tree_model = "%s %s.%s\n" % (label, section[1], iterator)
+
+    # This forces inserting the block at the very beginning (if there is), then
+    # the others.
+    content_types = tree_contents.keys()
+    if 'block' in content_types:
+        content, content_dim += make_content('block', tree_contents['block'], section)
+        tree_model += content
+        dim += content_dim
+
+    content_types.remove('block')
+    for t in content_types:
+        content, content_dim += make_content(t, tree_contents[t], section)
+        tree_model += content
+        dim += content_dim
+
+    return tree_model, dim
+
+
+# ===========================
+# Functions generating values
+# ===========================
+
+def make_exercise_values(number_exercises, dim):
+    """
+    """
+
+    values = []
+    for i in range(1, number_exercises + 1):
+        values.append( (i,) * dim )
+
+    return values
+
+
+# ========================
+# Functions printing stuff
+# ========================
+
+def print_heading(heading, depth):
+    """
+    """
+
+    prefix = depth * '*'
+    print('%s %s' % (prefix, heading))
+
+    
+def print_trees(tree_model, values, depth):
     """Prints 'tree_model' replacing its "variables" traversing every value in
     'values'.
-    
+
     tree_model: string
     values: tuple containing values replacing the "variables" in 'tree_model'
+    depth: positive integer
 
     """
 
     for v in values:
         print tree_model % v
+
         
+# ===================
+# Functions for cases
+# ===================
 
-def print_exercise_tree(sectioning, section_number, section_name, n_exercises, depth, files):
-    """Stampa l'albero:
-    1. Stampa il titolo : "sectioning" "section_number" - "section_name"
-    2. Stampa la stringa modello per ogni esercizio da 1 a 'n_exercises' : Exercise "section_number".%i (+ "blocco exercise")
-    3. Se 'files=True', stampa anche i percorsi a file associati all'esercizio.
-
-    >>> Esempio di albero <<<
-    
-    I dati vengono combinati tipo nell'esempio seguente:
-    1. sectioning = "Chapter"
-    2. section_number = "V"
-    3. section_name = "Simplicial homology"
-    4. n_exercises = 3
-
-    *** Stampa ***
-    Chapter V - Simplicial homology
-    * Exercise V.1
-    * Exercise V.2
-    * Exercise V.3
-
-    e in ciascun albero dell'esercizio è inserito un blocco "exercise" di
-    org-mode.
-
+def print_exercise_tree():
+    """
     """
 
-    # Stringa del titolo dell'albero degli esercizi, aggiustata con la
-    # profondità indicata in depth.
-    prefix = '*' * depth
-    title = "%s %s %s - %s" % (prefix, sectioning, section_number, section_name)
+    # Asks data to user
+    section = ask_section() # section_type, section_number, section_name
+    tree_contents = ask_contents # Blocks, links, etc.
+    number_exercises, depth, dim_1 = ask_printing_data()
 
-    # Scrive nella variabile 's' la "stringa modello", che è una stringa che
-    # contiene un '%i' al suo interno. Questo viene chiamato "iteratore". La
-    # stringa viene stampata 'n_exercises' (int > 0) volte e ogni volta al posto
-    # di iterator viene inserito un numero (partendo da 1 fino a 'n_exercises').
-    # La variabile 'iterator_instances' indica il numero di iteratori
-    # all'interno della stringa modello 's'. Questa informazione serve per poter
-    # dire alla funzione 'sequential_strings' quante volte compare l'iterator
-    # nella stringa modello.
+    # Makes string models
+    heading = make_heading(section)
+    # tree_heading, dim_2 = make_tree_heading('Exercise', section[1])
+    tree_model, dim_c = make_tree_model(section, tree_contents, 'Exercise')
+    dim = dim_c + dim_p
 
-    iterator_instances = 1 # Numero di istanze degli iteratori. A priori abbiamo solo un'istanza.
+    # Prints tree
+    values = make_exercise_values(number_exercises, dim)
+    print_heading(heading, depth)
+    print_trees(tree_model, values, depth)
+
+
+# def print_numbered_string():
+#     """
+#     """
+
+#     # Asks data to user
+#     string_model = 
     
-    prefix += '*' # Aggiunge una profondità a quella dichiarata in 'depth'.
-    
-    iterator = "%i"
-    s = ("%s Exercise %s." % (prefix, section_number)) + iterator + "\n"
-    s += "#+begin_exercise\n\n#+end_exercise\n"
 
-    # Se è richiesto di creare anche la stringa coi link ai file per gli
-    # esercizi (e.g. se sono esercizi di informatica).
-    if files:
-        file_name = section_name.translate({ord(c): None for c in ',;.:'}).lower().replace(' ', '-') # Rimuovo eventuale punteggiatura
-        iterator_instances += 1 # Aggiunge un'altra istanza dell'iteratore
-        s += ("\nLo script è file:~/informatica/linguaggi/python/think-python/exercise-%s-" % section_number.lower()) + iterator + ".py\n"
-        s += ("Soluzione: file:~/informatica/linguaggi/python/think-python/%s-%s-%s/\n" % (sectioning.lower(), section_number.lower(), section_name.lower()))
+#     print_trees(tree_model, values, depth)
 
-    # Stampa il titolo dell'albero
-    print(title)
 
-    # Stampa la stringa modello 's' sostituendo l'iterator per ogni intero da
-    # 'begin' (default è 1) a 'n_exercises'.
-    begin = 1
-    sequential_string(s, iterator_instances, begin, n_exercises)
-        
-    
-def make_exercise_tree():
-    """Prompt per chiedere:
-    1. Sectioning (i.e. chapter/appendix/section/subsection)
-    2. Numero del capitolo
-    3. Nome del capitolo
-    4. Numero esercizi
-    5. Profondità dell'albero
-    6. Se deve inserire percorsi a file dedicati agli esercizi 
-
-    Usando le informazioni, chiama 'print_exercise_tree' per stampare l'albero.
-
-    """
-
-    print("Insert 'c'/'a'/'s'/'ss' for chapter/appendix/section/subsection (default is 'c')")
-    print("Insert chapter/appendix/section/subsection number")
-    print("Insert name of the chapter/appendix/section/subsection")
-    print("Insert number of exercises")
-    print("Insert depth")
-    print("Create files (y/n)")
-    print("(Each information separted by spaces)\n")
-
-    sectioning_dict = {'c': 'Chapter', 'a': 'Appendix', 's': 'Section', 'ss': 'Subsection'}
-    files_dict = {'y': True, 'n': False}
-    
-    query = input("> ")
-    data = query.split()
-
-    sectioning = sectioning_dict[data[0]]
-    section_number = data[1]
-    section_name = ' '.join(data[2:-3])
-    exercises = data[-3]
-    depth = data[-2]
-    files = data[-1]
-
-    print_exercise_tree(sectioning, section_number, section_name, int(exercises), int(depth), files_dict[files])
-
+# ====================
+# Function asking case
+# ====================
 
 def prompt_command():
     """Fa il prompt di un comando.
@@ -146,16 +224,15 @@ def prompt_command():
         if query == '':
             break
         elif query in "exercise":
-            make_exercise_tree()
+            print_exercise_tree()
         elif query in "string":
             
         
     print("Goodbye.")
-    
+        
 
 # ======
 # Script
 # ======
 
 prompt_command()
-
